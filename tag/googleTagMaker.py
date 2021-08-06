4169385e88474fd4222363e5fc76d6d47cc5e361
tovfikur <tovfikurrahman@gmail.com>
2021-08-06 20:57:15 +0600

    7th Commit

--- tag/googleTagMaker.py
+++ tag/googleTagMaker.py
@@ -3,4 +3,4 @@ with open('2tagfile', 'r') as d2:
         with open('3tagfile', 'r') as d3:
             for k in d3.readlines():
                 with open(k, 'a') as d4:
-                    d4.write(j.replace('\n', '') + k.replace('\n', '') + 'map\n')
+                    d4.write(j.replace('\n', '') + k.replace('\n', '') + '\n')
