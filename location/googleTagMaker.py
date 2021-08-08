54aa495416c2433a0516cfe6962d4b5ad7f55675
tovfikur <tovfikurrahman@gmail.com>
2021-08-08 17:23:55 +0600

    Shohag vai recommendation

--- location/googleTagMaker.py
+++ location/googleTagMaker.py
@@ -1,6 +1,6 @@
-with open('2tagfile', 'r') as d2:
+with open('tag', 'r') as d2:
     for j in d2.readlines():
         with open('3tagfile', 'r') as d3:
             for k in d3.readlines():
                 with open(k, 'a') as d4:
-                    d4.write(j.replace('\n', '') + k.replace('\n', '') + '\n')
+                    d4.write(j.replace('\n', '') +' '+ k.replace('\n', '') + '\n')
